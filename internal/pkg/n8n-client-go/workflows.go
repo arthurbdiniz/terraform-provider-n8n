3181b9cd2f852c34f443fae9d6756d6fb3410549
Arthur Diniz <arthurbdiniz@gmail.com>
2025-02-17 23:47:23 +0000

    Implement pagination in GetWorkflows method
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- internal/pkg/n8n-client-go/workflows.go
+++ internal/pkg/n8n-client-go/workflows.go
@@ -14,10 +14,44 @@ type WorkflowsResponse struct {
 	NextCursor *string    `json:"nextCursor"` // Can be null in JSON, so use a pointer
 }
 
-// TODO: Make this method paginated using NextCursor
-// GetWorkflows - Returns list of workflows.
+// GetWorkflows - Retrieve all workflows from your instance.
 func (c *Client) GetWorkflows() (*WorkflowsResponse, error) {
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/v1/workflows", c.HostURL), nil)
+	var allWorkflows WorkflowsResponse
+	cursor := ""
+
+	for {
+		url := fmt.Sprintf("%s/api/v1/workflows", c.HostURL)
+		// Only append the cursor if it's not empty
+		if cursor != "" {
+			url = fmt.Sprintf("%s?cursor=%s", url, cursor)
+		}
+
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			return nil, err
+		}
+
+		body, err := c.doRequest(req, nil)
+		if err != nil {
+			return nil, err
+		}
+
+		var workflows WorkflowsResponse
+		err = json.Unmarshal(body, &workflows)
+		if err != nil {
+			return nil, err
+		}
+
+		allWorkflows.Data = append(allWorkflows.Data, workflows.Data...)
+		if workflows.NextCursor == nil {
+			break
+		}
+		cursor = *workflows.NextCursor
+	}
+
+	return &allWorkflows, nil
+}
+
 	if err != nil {
 		return nil, err
 	}
