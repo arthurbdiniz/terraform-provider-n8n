271c8b4d47f2cd122c79ebf22be0099fb4617d28
Arthur Diniz <arthurbdiniz@gmail.com>
2025-03-18 22:06:43 +0000

    Store default API token under config file
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

---
+++ internal/config/config.go
@@ -0,0 +1,6 @@
+// Copyright (c) Arthur Diniz <arthurbdiniz@gmail.com>
+// SPDX-License-Identifier: Apache-2.0
+
+package config
+
+var ApiToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyMWIyOWU4Ni04ZGNlLTRhYjQtODI5MC02OWJlZjQ3ZDE4OTEiLCJpc3MiOiJuOG4iLCJhdWQiOiJwdWJsaWMtYXBpIiwiaWF0IjoxNzQyMzM5NTA0fQ.BRv0dHE-TcryvLUvrMgjmpgKadm_E1OGNpvldIx43F0"
