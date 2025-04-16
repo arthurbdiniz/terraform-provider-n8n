cc5892c5d07d76788768762b95dc643b39a548d3
Arthur Diniz <arthurbdiniz@gmail.com>
2025-04-16 22:49:45 +0100

    Refactor provider workflows data source model and schema
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- internal/provider/workflows_data_source.go
+++ internal/provider/workflows_data_source.go
@@ -5,7 +5,6 @@ package provider
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 
 	"github.com/arthurbdiniz/terraform-provider-n8n/internal/pkg/n8n-client-go"
@@ -38,18 +37,17 @@ type workflowsDataSourceModel struct {
 
 // workflowsModel maps workflows schema data.
 type workflowsModel struct {
-	ID           types.String  `tfsdk:"id"`
-	Name         types.String  `tfsdk:"name"`
-	Active       types.Bool    `tfsdk:"active"`
-	VersionId    types.String  `tfsdk:"version_id"`
-	TriggerCount types.Int64   `tfsdk:"trigger_count"`
-	CreatedAt    types.String  `tfsdk:"created_at"`
-	UpdatedAt    types.String  `tfsdk:"updated_at"`
-	Nodes        []nodesModel  `tfsdk:"nodes"`
-	Connections  types.String  `tfsdk:"connections"`
-	Settings     settingsModel `tfsdk:"settings"`
-	Meta         metaModel     `tfsdk:"meta"`
-	Tags         []tagsModel   `tfsdk:"tags"`
+	ID           types.String   `tfsdk:"id"`
+	Name         types.String   `tfsdk:"name"`
+	Active       types.Bool     `tfsdk:"active"`
+	VersionId    types.String   `tfsdk:"version_id"`
+	TriggerCount types.Int64    `tfsdk:"trigger_count"`
+	CreatedAt    types.String   `tfsdk:"created_at"`
+	UpdatedAt    types.String   `tfsdk:"updated_at"`
+	Nodes        []nodesModel   `tfsdk:"nodes"`
+	Connections  types.String   `tfsdk:"connections"`
+	Settings     *settingsModel `tfsdk:"settings"`
+	Tags         []tagsModel    `tfsdk:"tags"`
 	// PinData      types.Map      `tfsdk:"pin_data"`
 	// StaticData   types.Map      `tfsdk:"static_data"`
 }
@@ -77,11 +75,14 @@ type parameterModel struct {
 }
 
 type settingsModel struct {
-	ExecutionOrder types.String `tfsdk:"execution_order"`
-}
-
-type metaModel struct {
-	TemplateCredsSetupCompleted types.Bool `tfsdk:"template_creds_setup_completed"`
+	SaveExecutionProgress    types.Bool   `tfsdk:"save_execution_progress"`
+	SaveManualExecutions     types.Bool   `tfsdk:"save_manual_executions"`
+	SaveDataErrorExecution   types.String `tfsdk:"save_data_error_execution"`
+	SaveDataSuccessExecution types.String `tfsdk:"save_data_success_execution"`
+	ExecutionTimeout         types.Int64  `tfsdk:"execution_timeout"`
+	ErrorWorkflow            types.String `tfsdk:"error_workflow"`
+	Timezone                 types.String `tfsdk:"timezone"`
+	ExecutionOrder           types.String `tfsdk:"execution_order"`
 }
 
 // Configure adds the provider configured client to the data source.
@@ -121,183 +122,45 @@ func (d *workflowsDataSource) Schema(_ context.Context, _ datasource.SchemaReque
 				NestedObject: schema.NestedAttributeObject{
 					Attributes: map[string]schema.Attribute{
 						"id": schema.StringAttribute{
-							Description: "Unique identifier of the workflow.",
 							Computed:    true,
+							Description: "Unique identifier of the workflow.",
 						},
 						"name": schema.StringAttribute{
-							Description: "Name of the workflow.",
 							Computed:    true,
+							Description: "Name of the workflow.",
 						},
 						"active": schema.BoolAttribute{
-							Description: "Indicates whether the workflow is currently active.",
 							Computed:    true,
+							Description: "Indicates whether the workflow is currently active.",
 						},
 						"version_id": schema.StringAttribute{
-							Description: "Identifier of the current version of the workflow.",
 							Computed:    true,
+							Description: "Identifier of the current version of the workflow.",
 						},
 						"trigger_count": schema.Int64Attribute{
-							Description: "Number of times the workflow has been triggered.",
 							Computed:    true,
+							Description: "Number of times the workflow has been triggered.",
 						},
 						"created_at": schema.StringAttribute{
-							Description: "Timestamp when the workflow was created.",
 							Computed:    true,
+							Description: "Timestamp when the workflow was created.",
 						},
 						"updated_at": schema.StringAttribute{
+							Computed:    true,
 							Description: "Timestamp when the workflow was last updated.",
-							Computed:    true,
 						},
-						"nodes": schema.ListNestedAttribute{
-							Description: "List of nodes in the workflow.",
-							Computed:    true,
-							NestedObject: schema.NestedAttributeObject{
-								Attributes: map[string]schema.Attribute{
-									"id": schema.StringAttribute{
-										Description: "Node identifier.",
-										Computed:    true,
-									},
-									"name": schema.StringAttribute{
-										Description: "Node name.",
-										Computed:    true,
-									},
-									"type": schema.StringAttribute{
-										Description: "Type of the node.",
-										Computed:    true,
-									},
-									"type_version": schema.Float64Attribute{
-										Description: "Version of the node type.",
-										Computed:    true,
-									},
-									"position": schema.ListAttribute{
-										Description: "Position of the node in the workflow.",
-										Computed:    true,
-										ElementType: types.Int64Type,
-									},
-									"parameters": schema.ListNestedAttribute{
-										Description: "Parameters of the node.",
-										Computed:    true,
-										NestedObject: schema.NestedAttributeObject{
-											Attributes: map[string]schema.Attribute{
-												"key": schema.StringAttribute{
-													Description: "The parameter key.",
-													Computed:    true,
-												},
-												"type": schema.StringAttribute{
-													Description: "The type of the value.",
-													Computed:    true,
-												},
-												"value": schema.StringAttribute{
-													Description: "The value as a string.",
-													Computed:    true,
-												},
-											},
-										},
-									},
-								},
-							},
-						},
+						"nodes": workflowsNodeAttr(),
 						"connections": schema.StringAttribute{
+							Computed:    true,
 							Description: "Raw JSON representation of connections between nodes.",
-							Computed:    true,
 						},
-						"settings": schema.SingleNestedAttribute{
-							Description: "Workflow settings.",
-							Computed:    true,
-							Attributes: map[string]schema.Attribute{
-								"execution_order": schema.StringAttribute{
-									Description: "Execution order setting.",
-									Computed:    true,
-								},
-							},
-						},
-						"meta": schema.SingleNestedAttribute{
-							Description: "Workflow metadata.",
-							Computed:    true,
-							Attributes: map[string]schema.Attribute{
-								"template_creds_setup_completed": schema.BoolAttribute{
-									Description: "Indicates if credentials setup is completed.",
-									Computed:    true,
-								},
-							},
-						},
-						"tags": schema.ListNestedAttribute{
-							Description: "Tags associated with the workflow.",
-							Computed:    true,
-							NestedObject: schema.NestedAttributeObject{
-								Attributes: map[string]schema.Attribute{
-									"created_at": schema.StringAttribute{
-										Description: "",
-										Computed:    true,
-									},
-									"updated_at": schema.StringAttribute{
-										Description: "",
-										Computed:    true,
-									},
-									"id": schema.StringAttribute{
-										Description: "",
-										Computed:    true,
-									},
-									"name": schema.StringAttribute{
-										Description: "",
-										Computed:    true,
-									},
-								},
-							},
-						},
+						"settings": workflowsSettingsAttr(),
+						"tags":     workflowsTagsAttr(),
 					},
 				},
 			},
 		},
 	}
-}
-
-func ConvertToTerraformList(input map[string]interface{}) ([]parameterModel, error) {
-	var paramList []parameterModel
-
-	// Iterate over the input map to convert each element
-	for k, v := range input {
-		var valueStr string
-		var valueType string
-
-		// Determine the type of the value and convert it to string
-		switch val := v.(type) {
-		case string:
-			valueType = "string"
-			valueStr = val
-		case int:
-			valueType = "int"
-			valueStr = fmt.Sprintf("%d", val)
-		case float64:
-			valueType = "float"
-			valueStr = fmt.Sprintf("%f", val)
-		case bool:
-			valueType = "bool"
-			valueStr = fmt.Sprintf("%t", val)
-		default:
-			valueType = "unknown"
-			valueStr = fmt.Sprintf("%v", val)
-		}
-
-		// Append the parameter to the list
-		paramList = append(paramList, parameterModel{
-			Key:   types.StringValue(k),
-			Type:  types.StringValue(valueType),
-			Value: types.StringValue(valueStr),
-		})
-	}
-
-	return paramList, nil
-}
-
-func ConvertConnectionsToTerraformMap(connections interface{}) (types.String, error) {
-	data, err := json.Marshal(connections) // Convert to JSON string
-
-	if err != nil {
-		return types.StringNull(), err
-	}
-
-	return types.StringValue(string(data)), nil
 }
 
 // Read refreshes the Terraform state with the latest data.
@@ -323,7 +186,6 @@ func (d *workflowsDataSource) Read(ctx context.Context, req datasource.ReadReque
 			for _, pos := range node.Position {
 				positions = append(positions, types.Int64Value(int64(pos)))
 			}
-			tflog.Info(ctx, "test")
 
 			params, err := ConvertToTerraformList(node.Parameters)
 			if err != nil {
@@ -333,9 +195,6 @@ func (d *workflowsDataSource) Read(ctx context.Context, req datasource.ReadReque
 				continue // Skip this node if there's an error
 			}
 
-			tflog.Info(ctx, "test2")
-			str, _ := json.Marshal(node)
-			tflog.Info(ctx, string(str))
 			nodes = append(nodes, nodesModel{
 				ID:          types.StringValue(node.ID),
 				Name:        types.StringValue(node.Name),
@@ -344,7 +203,6 @@ func (d *workflowsDataSource) Read(ctx context.Context, req datasource.ReadReque
 				Parameters:  params,
 				Position:    positions,
 			})
-			tflog.Info(ctx, "here")
 		}
 
 		// Convert tags
@@ -378,11 +236,15 @@ func (d *workflowsDataSource) Read(ctx context.Context, req datasource.ReadReque
 			UpdatedAt:    types.StringValue(workflow.UpdatedAt),
 			Nodes:        nodes,
 			Connections:  connectionsJSON,
-			Settings: settingsModel{
-				ExecutionOrder: types.StringValue(workflow.Settings.ExecutionOrder),
-			},
-			Meta: metaModel{
-				TemplateCredsSetupCompleted: types.BoolValue(workflow.Meta.TemplateCredsSetupCompleted),
+			Settings: &settingsModel{
+				SaveExecutionProgress:    types.BoolValue(workflow.Settings.SaveExecutionProgress),
+				SaveManualExecutions:     types.BoolValue(workflow.Settings.SaveManualExecutions),
+				SaveDataErrorExecution:   types.StringValue(workflow.Settings.SaveDataErrorExecution),
+				SaveDataSuccessExecution: types.StringValue(workflow.Settings.SaveDataSuccessExecution),
+				ExecutionTimeout:         types.Int64Value(int64(workflow.Settings.ExecutionTimeout)),
+				ErrorWorkflow:            types.StringValue(workflow.Settings.ErrorWorkflow),
+				Timezone:                 types.StringValue(workflow.Settings.Timezone),
+				ExecutionOrder:           types.StringValue(workflow.Settings.ExecutionOrder),
 			},
 			Tags: tags,
 		}
